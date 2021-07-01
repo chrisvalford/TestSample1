aec310f7d0dd87253e66b4d3140b95036d7c24ef
Christopher Alford <chrisvalford@me.com>
2021-07-01 12:41:41 +0200

    Initial commit

--- TestSample1/main.swift
+++ TestSample1/main.swift
@@ -36,5 +36,6 @@ public func solution(_ a: inout [Int]) -> Int {
     return -1
 }
 
-var A = [-1,-2,0]
+// Try any integer between + or 1 1 million
+var A = [-1, -2, 0, 1, 2, 999999, 3]
 print(solution(&A))
